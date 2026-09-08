113a44edcbd00d46bf920637a1a87bde604e99a9
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2026-09-08 13:41:09 +0100

    Add files via upload

--- TB/reg_tb.sv
+++ TB/reg_tb.sv
@@ -9,7 +9,7 @@ logic [31:0] write_data;
 logic [1:0] shape_type;
 logic [3:0] addr;
 logic enable;
-logic [6:0] filler;
+logic [11:0] color;
 
 register_system dut (
     .clk(clk),
@@ -17,9 +17,13 @@ register_system dut (
     .x(x),
     .y(y),
     .w(w),
+    .h(h),
+    .reset(reset),
     .write_data(write_data),
     .write_enable(write_enable),
-    .enable(enable)
+    .enable(enable),
+    .color(color),
+    .shape_type(shape_type)
 );
 
 initial 
@@ -33,14 +37,14 @@ reset = 1;
 addr = 4'h0;
 write_enable = 1'b0;
 write_data = 32'h0;
-filler = 6'h00;
 
 #5 
 reset = 0;
 
 #10
 addr = 4'h8;
-write_data = {17'b0, 2'b01, 1'b1, 12'hFFF};
+//write_data = {17'b0, 2'b01, 1'b1, 12'hFFF};
+write_data = {16'h3FF,16'h3FF};
 write_enable = 1;
 
 #10
@@ -48,10 +52,10 @@ write_enable = 0;
 
 
 #10
-    if (color == 12'hFFF)
+    if (h == 10'h3FF)
     $display("Successful");
     else
-        $display("FAIL: w =%h, expected 0x32", w);
+        $display("FAIL: w =%h", h);
     $finish;
   end
 endmodule
